06a744b938ebc303a5c79b0b5c1fea40d3b48fa4
Chai Roos <elchairoos15@gmail.com>
2026-06-13 18:44:52 +0300

    Fix typo and rename SmartQeue to SmartQueue, remove trailing backslash in imports

--- source_code/smartQeue.py
+++ source_code/smartQeue.py
@@ -1,56 +1,54 @@
 from typing import List, Tuple
 
 
-class SmartQeue:
+class SmartQueue:
     def __init__(self) -> None:
         """
         ::
-            Constructor for SmartQeue.
-            The holds tuples representing points (x, y).
+            Constructor for SmartQueue.
+            Holds tuples representing points (x, y).
             Every 2 points compose a line.
         """
-        self.qeue: List[Tuple[int, int]] = []
+        self.queue: List[Tuple[int, int]] = []
 
     def append(self, value: Tuple[int, int]) -> None:
         """
         ::
-            Appends to the beinning of the qeue (head).
-
+            Appends to the beginning of the queue (head).
 
         Parameters:
             (Tuple[int, int]) value:        Value to append.
         """
-        self.qeue.insert(0, value)
+        self.queue.insert(0, value)
 
     def push(self, value: Tuple[int, int]) -> None:
         """
         ::
-            Pushes to the left side of the qeue, FIFO style.
+            Pushes to the left side of the queue, FIFO style.
 
         Parameters:
             (Tuple[int, int]) value:        Value to push.
         """
         self.append(value)
-        self.qeue.pop()
+        if self.queue:
+            self.queue.pop()
 
     def get_head_point(self) -> Tuple[int, int]:
         """
         ::
-            Getter for first value int qeue.
+            Getter for first value in queue.
 
         Return:
-            (Tuple[int, int]):                          First element in qeue.
-
+            (Tuple[int, int]):                          First element in queue.
         """
-        return self.qeue[0]
+        return self.queue[0]
 
     def get_head_node(self) -> Tuple[Tuple[int, int], Tuple[int, int]]:
         """
         ::
-            Getter for first 2 elements in qeue as a tuple.
+            Getter for first 2 elements in queue as a tuple.
 
         Return:
             (Tuple[Tuple[int, int], Tuple[int, int]]):  First 2 elements as tuple.
-
         """
-        return self.qeue[0], self.qeue[1]
+        return self.queue[0], self.queue[1]
